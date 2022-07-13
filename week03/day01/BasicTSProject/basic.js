0c10e45f98c9fdd91fd2e953420fef6363fa6ef4
attilakrupl <krupl.attila@gmail.com>
2022-07-13 23:35:21 +0200

    first ts stuff

---
+++ week03/day01/BasicTSProject/basic.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+var message = "Yo bro!";
+console.log(message);
+var x;
+x = 5;
+var y = 10;
+// y = 15; - error
+var isBeginner = true;
+var name = 'Attila';
+var intro = "My name is ".concat(name, " and im a beginner");
+console.log(intro);
+// interface String {
+//     toJadenCase(): string;
+// }
+// String.prototype.toJadenCase = function () {
+//     return this.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+// };
---
+++ week03/day01/BasicTSProject/basic.ts
@@ -0,0 +1,26 @@
+export {}
+
+let message = "Yo bro!";
+console.log(message);
+
+let x : number;
+x = 5;
+
+const y : number = 10;
+// y = 15; - error
+
+let isBeginner: boolean = true;
+let name: string = 'Attila';
+let intro: string = `My name is ${name} and im a beginner`;
+
+console.log(intro);
+
+// interface String {
+//     toJadenCase(): string;
+// }
+
+// String.prototype.toJadenCase = function () {
+//     return this.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+// };
+
+
